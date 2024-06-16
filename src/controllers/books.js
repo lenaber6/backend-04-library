60c721a1c1e6518dac7ac882fdf6415b9b667987
lenaber6 <lenaber6@mail.ru>
2024-06-16 09:54:49 +0300

    не scheme

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -4,33 +4,34 @@ const bookSchema = require("../models/book");
 
 const getBooks = (request, response) => {
   // Get all books
-  if (request.schema === bookSchema) {
   return Book.find({})
     .then((data) => {
+      if (!data) {
+        response.status(404).send("Такие книги не найдены");
+      }
       response.status(200).send(data);
     })
     .catch((e) => {
       response.status(500).send(e.message);
     });
-  } else {
-    response.status(404).send("Такие книги не найдены");
-  }
 };
+
+
 const getBook = (request, response) => {
   // Get a book by id
   const { book_id } = request.params;
-  if (request.schema === bookSchema) {
   return Book.findById(book_id)
     .then((book) => {
+      if (!book) {
+        response.status(404).send("Книга не найдена");
+      }
       response.status(200).send(book);
     })
     .catch((e) => {
       response.status(500).send(e.message);
     });
-  } else {
-    response.status(404).send("Книга не найдена");
-  }
 };
+
 
 const createBook = (request, response) => {
   // Create new book
@@ -47,36 +48,38 @@ const updateBook = (request, response) => {
   // Update a book by id
   const { book_id } = request.params;
   const data = { ...request.body };
-  if (request.schema === bookSchema) {
   return Book.findByIdAndUpdate(book_id, data, {
     new: true,
     runValidators: true,
   })
     .then((book) => {
+      if (!book) {
+        response.status(404).send("Книга не найдена");
+      }
       response.status(200).send(book);
     })
     .catch((e) => {
       response.status(500).send(e.message);
     });
-  } else {
-    response.status(404).send("Книга не найдена");
-  }
 };
+
+
 const deleteBook = (request, response) => {
   // Delete a book by id
   const { book_id } = request.params;
-  if (request.schema === bookSchema) {
   return Book.findByIdAndDelete(book_id)
     .then((book) => {
+      if (!book) {
+        response.status(404).send("Книга не найдена");
+      }
       response.status(200).send("Book deleted");
     })
     .catch((e) => {
       response.status(500).send(e.message);
     });
-  } else {
-    response.status(404).send("Книга не найдена");
-  }
 };
+
+
 module.exports = {
   getBooks,
   getBook,
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -3,33 +3,35 @@ const readerSchema = require("../models/reader");
 
 const getReaders = (request, response) => {
   // Get all readers
-  if (request.schema === readerSchema) {
+  // if (request.schema === readerSchema) {   // В такой логике некорректно отдаёт читателей/книги, которые на самом деле есть.
     return Reader.find({})
       .then((data) => {
+        if (!data) { // Если результат вызова null, то возвращаем статус 404
+          response.status(404).send("Читатели не найдены"); 
+        }
         response.status(200).send(data);
       })
       .catch((e) => {
         response.status(500).send(e.message);
       });
-  } else {
-    response.status(404).send("Читатели не найдены");
-  }
+  // } else {
+  //   response.status(404).send("Читатели не найдены");
+  // }
 };
 
 const getReader = (request, response) => {
   const { reader_id } = request.params;
-  if (request.schema === readerSchema) {
     return Reader.findById(reader_id)
       .then((user) => {
+        if(!user) {
+          response.status(404).send("Читатель не найден");
+        }
         response.status(200).send(user);
       })
       .catch((e) => {
         response.status(500).send(e.message);
       });
     // response.send(`Reader with id: ${reader_id}`);
-  } else {
-    response.status(404).send("Читатель не найден");
-  }
 };
 
 const createReader = (request, response) => {
@@ -49,36 +51,34 @@ const updateReader = (request, response) => {
   // Update reader
   const { reader_id } = request.params;
   const data = request.body;
-  if (request.schema === readerSchema) {
     return Reader.findByIdAndUpdate(reader_id, data, {
       new: true,
       runValidators: true,
     })
       .then((user) => {
+        if (!user) {
+          response.status(404).send("Читатель не найден");
+        }
         response.status(200).send(user);
       })
       .catch((e) => {
         response.status(500).send(e.message);
       });
-  } else {
-    response.status(404).send("Читатель не найден");
-  }
 };
 
 const deleteReader = (request, response) => {
   // Delete reader
   const { reader_id } = request.params;
-  if (request.schema === readerSchema) {
     return Reader.findByIdAndDelete(reader_id)
       .then((user) => {
+        if (!user) {
+          response.status(404).send("Читатель не найден");
+        }
         response.status(200).send("Reader deleted");
       })
       .catch((e) => {
         response.status(500).send(e.message);
       });
-  } else {
-    response.status(404).send("Читатель не найден");
-  }
 };
 
 module.exports = {
